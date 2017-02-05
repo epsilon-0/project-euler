5764b38550b3b2bab40f09199cb68d9aad4d4ed2
epsilon-0 <mathgeniusabhinav@gmail.com>
2017-02-05 16:35:09 -0500

    solved 22

--- 22/22.cc
+++ 22/22.cc
@@ -2,10 +2,32 @@
 
 using namespace std;
 
+int scoreOfName(string name){
+  int score = 0;
+  for(char c : name)
+    score += c - 'A' + 1;
+  return score;
+}
+
 int main() {
-  int t;
-  cin >> t;
-  while(t--){
-    
+  int n;
+  cin >> n;
+  vector<string> names;
+  while(n--){
+    string name;
+    cin >> name;
+    names.push_back(name);
+  }
+  sort(names.begin(), names.end());
+  map<string, int> scores;
+  for(int i = 0; i < names.size(); i++){
+    scores[names[i]] = (i+1) * scoreOfName(names[i]);
+  }
+  int q;
+  cin >> q;
+  while(q--){
+    string name;
+    cin >> name;
+    cout << scores[name] << endl;
   }
 }
