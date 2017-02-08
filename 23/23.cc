36574b80b1a2ad93dfa5b1f8a99da952643b50f4
epsilon-0 <mathgeniusabhinav@gmail.com>
2017-02-08 15:28:45 -0500

    solved 23

--- 23/23.cc
+++ 23/23.cc
@@ -2,36 +2,49 @@
 
 using namespace std;
 
-int factor[100005], sumOfFactors[100005];
+const int MAXN = 29000;
+
+int factor[MAXN], sumOfFactors[MAXN];
 
 int main() {
-  for(int i = 0; i < 100005; i++)
+  for(int i = 0; i < MAXN; i++)
     factor[i] = sumOfFactors[i] = -1;
   factor[0] = factor[1] = 1;
-  for(int i = 2; i < sqrt(100005) + 5; i++)
+  for(int i = 2; i < sqrt(MAXN) + 5; i++)
     if(factor[i] == -1)
-      for(int j = i*i; j < 100005; j += i)
+      for(int j = i*i; j < MAXN; j += i)
         if(factor[j] == -1)
           factor[j] = i;
   
   sumOfFactors[0] = 0; sumOfFactors[1] = 1;
-  for(int i = 2; i < 100005; i++)
+  for(int i = 2; i < MAXN; i++)
     if(factor[i] == -1)
-      sumOfFactors[i] = 1;
-    else
-      sumOfFactors[i] = factor[i]*(sumOfFactors[i/factor[i]]);
+      sumOfFactors[i] = i+1;
+    else{
+      int prod = 1;
+      int sum = 1;
+      int kkk = i;
+      while(kkk % factor[i] == 0){
+        prod *= factor[i];
+        sum += prod;
+        kkk /= factor[i];
+      }
 
-  for(int i = 0; i < 100; i++)
-    cout << i << " " << sumOfFactors[i] << endl;
+      sumOfFactors[i] = sum * sumOfFactors[kkk];
+    }
 
   int t;
   cin >> t;
   while(t--){
     int n;
     cin >> n;
+    if(n > 28123){
+      cout << "YES\n";
+      continue;
+    }
     bool isSum = false;
     for(int i = 1; i < n; i++)
-      if(factor[i] != -1 && sumOfFactors[i] > i && sumOfFactors[n-i] > n-i){
+      if(factor[i] != -1 && sumOfFactors[i] > 2*i && sumOfFactors[n-i] > 2*(n-i)){
         isSum = true;
         break;
       }
