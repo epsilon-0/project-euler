253e9d6f61fe5d4718c8a70c4c2b29e07db1eaed
epsilon-0 <mathgeniusabhinav@gmail.com>
2017-02-12 21:28:57 -0500

    initializing files for 25

--- 25/25.py
+++ 25/25.py
@@ -1,19 +1,58 @@
-numTests = int(raw_input())
+fib_matrix = [[1,1],
+              [1,0]]
 
-l = [0,1]
+def matrix_square(A):
+    return mat_mult(A,A)
 
-mappedList = dict()
-mappedList[1] = 1
 
-countt = 2
+def mat_mult(A,B):
+  return [[(A[0][0]*B[0][0] + A[0][1]*B[1][0]), (A[0][0]*B[0][1] + A[0][1]*B[1][1])],
+            [(A[1][0]*B[0][0] + A[1][1]*B[1][0]), (A[1][0]*B[0][1] + A[1][1]*B[1][1])]]
 
-while(str(l[-1]) != 5000):
-  l.append(l[-1] + l[-2])
-  l.pop(0)
-  if len(str(l[-1])) not in mappedList:
-    mappedList[len(str(l[-1]))] = countt
-  countt += 1
 
-for tt in range(numTests):
-  n = int(raw_input())
-  print(mappedList[n])
+def matrix_pow(M, power):
+    if power <= 0:
+      return M
+
+    powers =  list(reversed([True if i=="1" else False for i in bin(power)[2:]]))
+
+    matrices = [None for _ in powers]
+    matrices[0] = M
+
+    for i in range(1,len(powers)):
+        matrices[i] = matrix_square(matrices[i-1])
+
+
+    result = None
+
+    for matrix, power in zip(matrices, powers):
+        if power:
+            if result is None:
+                result = matrix
+            else:
+                result = mat_mult(result, matrix)
+
+    return result
+
+def fibonacci(n):
+  if(n == 0):
+    return 0
+  else:
+    return matrix_pow(fib_matrix, n)[0][1]
+
+print(fibonacci(23922))
+
+# numTests = int(input())
+
+# for tt in range(numTests):
+#   hi = 24000
+#   lo = 1
+#   n = int(input())
+#   while(hi != lo):
+#     mid = (hi+lo)//2
+#     length = len(str(fibonacci(mid)))
+#     if(length >= n):
+#       hi = mid
+#     else:
+#       lo = mid+1
+#   print(hi)
